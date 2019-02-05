10d897af4d9644249adcc7ff15d31c668592c4c2
ZeroAbsolue <nkouekamwilfried@gmail.com>
2019-02-05 18:18:39 +0100

    update appServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
        Schema::defaultStringLength (191);
 
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
             'Salle' => 'App\Model\Salle',
             'Chambre' => 'App\Model\Chambre',
         ]);
+
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
     }
 
     /**
@@ -31,6 +36,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
