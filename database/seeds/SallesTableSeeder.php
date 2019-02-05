6dc5167c73c19ac045953ca47812fb31e2e061af
ZeroAbsolue <nkouekamwilfried@gmail.com>
2019-02-05 21:23:12 +0100

    update seeder chambresTableSeeder and SallesTableSeeder

--- database/seeds/SallesTableSeeder.php
+++ database/seeds/SallesTableSeeder.php
@@ -11,23 +11,20 @@ class SallesTableSeeder extends Seeder
      */
     public function run()
     {
-        $salles = Array(
-            "0"=>Array(
-                "type"=>"CONFERENCE",
-                "name"=>"SALLE DE CONFÉRENCE",
-                "area"=>"25*15",
-                "sitting_place"=>"200",
-            ),
-            "1"=>Array(
-                "type"=>"BANQUET",
-                "name"=>"SALLE DE FÊTE",
-                "area"=>"15*50",
-                "sitting_place"=>"300",
-            ),
-        );
 
-        foreach ($salles as $salle) {
-            factory(App\Model\Salle::class, 1)->create($salle);
-        }
+        DB::table('Salles')->insert([
+            "type"=>"CONFERENCE",
+            "name"=>"SALLE DE CONFÉRENCE",
+            "area"=>"25*15",
+            "sitting_place"=>"200",
+        ]);
+
+        DB::table('Salles')->insert([
+            "type"=>"BANQUET",
+            "name"=>"SALLE DE FÊTE",
+            "area"=>"15*50",
+            "sitting_place"=>"300",
+        ]);
+
     }
 }
